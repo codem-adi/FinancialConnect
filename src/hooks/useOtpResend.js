4614a4f9f61c5ab511d0ff1cc7c7602f3a28904f
aditya.singh <emails4adi@gmail.com>
2026-06-22 02:11:39 +0530

    otp changes

--- src/hooks/useOtpResend.js
+++ src/hooks/useOtpResend.js
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { fetchOtpStatus } from '../lib/api';
+import { fetchOtpStatus } from '../lib/apiOtp';
 import { formatOtpWait } from '../lib/otpResend';
 
 /**
